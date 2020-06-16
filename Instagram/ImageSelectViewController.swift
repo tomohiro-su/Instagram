4c95e297a25e00f837c9094e420e764b1eeea286
tomohiro.suda <egran39@gmail.com>
2020-06-16 16:45:23 +0900

    はじめの記入、firebaseと未連動

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by 須田　知弘 on 2020/06/14.
+//  Copyright © 2020 tomohiro.suda. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 須田　知弘 on 2020/06/14.
+//  Copyright © 2020 tomohiro.suda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
